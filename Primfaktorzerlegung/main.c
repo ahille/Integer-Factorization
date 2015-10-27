a1ffd2ddf93b641a32c8660a246f22015c0f2ccb
Adrian K. Hille <adrian-hille@web.de>
2015-10-27 09:09:29 +0100

    Initial Commit

---
+++ Primfaktorzerlegung/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Primfaktorzerlegung
+//
+//  Created by HILLETECH on 27.10.15.
+//  Copyright © 2015 HILLETECH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
